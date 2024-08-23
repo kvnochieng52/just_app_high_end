6ebc05bff3f44fdfc97bd58520f48e2bd0259d66
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2024-08-23 11:45:10 +0300

    email start

--- resources/views/mailing/agent_email.blade.php
+++ resources/views/mailing/agent_email.blade.php
@@ -1,1 +1,11 @@
-Test
+<p><b>Hello</b>,</p>
+<p>You have new email request from <b>{{$property_title}} </b>Details are below: </p>
+<p><b>Client Email:</b> {{$name}}</p>
+<p><b>Telephone:</b> {{$telephone}}</p>
+<p><b>Email:</b> {{$email}}</p>
+<p><b>Message:</b><br />
+
+    {{$message}}
+</p>
+
+<p>This email was sent from justhomes.co.ke. </p>
