e88887e74698698cbc39e43326bc8651779b155c
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2024-09-16 17:08:12 +0300

    offplan and auction search

--- app/Http/Controllers/Api/PropertyController.php
+++ app/Http/Controllers/Api/PropertyController.php
@@ -209,13 +209,11 @@ class PropertyController extends Controller
                 }
 
 
-                dd($request['selectedFeatures']);
                 if (!empty($request['selectedFeatures'])) {
-                    // Decode JSON data
-                    $selectedFeatures = json_decode($request['selectedFeatures'], true);
+                    // selectedFeatures is already an array
+                    $selectedFeatures = $request['selectedFeatures'];
 
-
-                    // Check if decoding was successful and that selectedFeatures is an array
+                    // Check if selectedFeatures is an array
                     if (is_array($selectedFeatures)) {
                         PropertySelectedFeauture::where('property_id', $request['propertyID'])->delete();
                         foreach ($selectedFeatures as $feature) {
