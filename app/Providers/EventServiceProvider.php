3466fb27ebffaf07b450b39018bde3464651db85
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2024-10-03 14:39:24 +0300

    apple endpoint

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -17,12 +17,13 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+            \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+                \SocialiteProviders\Apple\AppleExtendSocialite::class . '@handle',
+            ],
 
         ],
 
-        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
-            \SocialiteProviders\Apple\AppleExtendSocialite::class . '@handle',
-        ],
+
 
 
     ];
