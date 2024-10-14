cd2fb368a0e9e145eebb2439ad519b587d79c910
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2024-10-14 07:45:16 +0300

    land types addition

---
+++ app/Models/LandMeasurement.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LandMeasurement extends Model
+{
+    use HasFactory;
+}
