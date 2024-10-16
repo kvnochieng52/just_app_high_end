b1173941990ecbf46c6c8ac76a282f8411d37532
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2024-10-16 08:40:02 +0300

    Reelsupload

--- app/Models/ReelVideo.php
+++ app/Models/ReelVideo.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class ReelVideo extends Model
 {
     use HasFactory;
+
+    public function comments()
+    {
+        return $this->hasMany(ReelComment::class, 'video_id', 'id');
+    }
 }
