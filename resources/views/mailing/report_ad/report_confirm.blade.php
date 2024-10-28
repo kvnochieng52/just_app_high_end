4da92f24dcc9d9b95f1f2ee662aa43bf5eec5782
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2024-10-28 18:46:54 +0300

    Subpks

--- resources/views/mailing/report_ad/report_confirm.blade.php
+++ resources/views/mailing/report_ad/report_confirm.blade.php
@@ -1,6 +1,6 @@
 <p>Dear,</p>
 
-<p>This is to confirm we have received your email on the Ad: {{ $property_name }} at Location {{{{ $location }}}}. Our
+<p>This is to confirm we have received your email on the Ad: {{ $property_name }} at Location {{ $location }}. Our
     Staff will review your request and give you feedback within 24 Hrs.</p>
 
 <p>Thanks & Regards,</p>
