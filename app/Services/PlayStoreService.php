e289c4b6cdda8bfe702d8d2724de000d09e4580c
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2025-01-30 16:53:58 +0300

    google play store

--- app/Services/PlayStoreService.php
+++ app/Services/PlayStoreService.php
@@ -6,7 +6,7 @@ class PlayStoreService
 {
     public function getAppDownloads($appId)
     {
-        $scriptPath = base_path('node-scripts/getDownloads.js'); // Change .js to .mjs
+        $scriptPath = base_path('node-scripts/getDownloads.mjs'); // Change .js to .mjs
         $command = "node " . escapeshellarg($scriptPath) . " " . escapeshellarg($appId);
         return trim(shell_exec($command)); // Execute and return output
     }
