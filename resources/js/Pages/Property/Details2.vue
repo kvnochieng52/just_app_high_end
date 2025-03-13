3c3341894c2082061a6eac4cd4141363b6613f22
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2025-03-13 14:55:23 +0300

    dashboard started

--- resources/js/Pages/Property/Details2.vue
+++ resources/js/Pages/Property/Details2.vue
@@ -100,7 +100,10 @@
                         </div>
                       </div>
 
-                      <div class="col-md-3 text-center">
+                      <div
+                        class="col-md-3 text-center"
+                        v-if="!propertyDetails.parking_spaces"
+                      >
                         <div class="counter-status md-mb-0">
                           <div
                             class="counter-icon"
