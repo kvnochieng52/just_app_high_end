954a7d6644494e847248ba931c2013baa876d4de
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2025-03-20 14:05:52 +0300

    bugs fixes

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -69,11 +69,12 @@
 
             <li>
               <a href="javascript:void(0);"
-                >Rent <span class="fa fa-caret-down m-0"></span
+                >Rents <span class="fa fa-caret-down m-0"></span
               ></a>
               <ul class="sub-menu">
                 <li aria-haspopup="true">
-                  <a href="/search/?search=1&leaseType=1&propertyType[]=1"
+                  <a
+                    href="https://justhomes.co.ke/search/?search=1&leaseType=1&propertyType[]=1"
                     >Apartment</a
                   >
                 </li>
