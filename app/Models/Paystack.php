b2eec79fe19264dc5f9c3c423a80a69af37ecb76
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2025-04-20 14:05:02 +0300

    bulk options

--- app/Models/Paystack.php
+++ app/Models/Paystack.php
@@ -22,18 +22,32 @@ class Paystack extends Model
         $paystackPublicKey = "pk_live_d0572a92a218942399656df9280dc12f0d0feb5f";
         $paystackSecretKey = "sk_live_d05f976d0f08c94ef8587b3dd59ccc1274a54e90";
 
-        $callbackUrl = "https://justhomes.co.ke/paystack/callback";
+        //  $callbackUrl = "https://justhomes.co.ke/paystack/callback";
 
+
+        $callbackUrl = env('APP_URL') . "/paystack/callback";
 
         // $email = $_POST["email"];
         $amount = $amount * 100; // Convert to kobo (Paystack uses kobo)
+
+
+        $country = env('COUNTRY');
+
+
+        if ($country == 'KENYA') {
+            $currency = env('CURRENCY', 'KES');
+            $amount = $amount;
+        } else {
+            $currency = 'USD';
+            $amount = self::convertCurrency(env('CURRENCY'), $amount,  'USD');
+        }
 
 
 
         $postData = [
             "email" => $email,
             "amount" => $amount,
-            "currency" => env('CURRENCY', 'KES'),
+            "currency" => $currency,
             "callback_url" => $callbackUrl,
         ];
 
@@ -54,5 +68,30 @@ class Paystack extends Model
         $result = json_decode($response, true);
 
         return $result;
+    }
+
+
+
+
+    public static function convertCurrency($fromCurrency, $amount, $toCurrency = 'USD')
+    {
+        // Example exchange rates relative to 1 USD
+        $exchangeRates = [
+            'NGN' => 1400.00,
+            'TZS' => 2500.00,
+            'UGX' => 3800.00,
+            'USD' => 1.00,
+        ];
+
+        // Check if both currencies exist in our rates list
+        if (!isset($exchangeRates[$fromCurrency]) || !isset($exchangeRates[$toCurrency])) {
+            return "Unsupported currency.";
+        }
+
+        // Convert from base currency to USD first, then to target currency
+        $amountInUSD = $amount / $exchangeRates[$fromCurrency];
+        $convertedAmount = $amountInUSD * $exchangeRates[$toCurrency];
+
+        return round($convertedAmount, 2);
     }
 }
